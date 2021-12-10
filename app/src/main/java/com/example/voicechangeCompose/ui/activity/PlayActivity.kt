b44959bf0dc39b931a9ae2e000b3185b32fafe63
icestar <csg@qq.com>
2021-12-10 17:08:38 +0800

    review code

--- app/src/main/java/com/example/voicechangeCompose/ui/activity/PlayActivity.kt
+++ app/src/main/java/com/example/voicechangeCompose/ui/activity/PlayActivity.kt
@@ -1,29 +1,15 @@
 package com.example.voicechangeCompose.ui.activity
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.media.AudioFormat
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.core.view.WindowCompat
-import com.example.voicechangeCompose.audio.common.PlayState
-import com.example.voicechangeCompose.module.Utils
 import com.example.voicechangeCompose.view.AudioPlayScreen
 import com.example.voicechangeCompose.viewmodel.MainViewModel
-import com.voicechange.audio.SampleAudioPlayer
-import com.voicechange.audio.common.AudioParam
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileNotFoundException
-import java.io.IOException
 
 
 class PlayActivity: ComponentActivity(){
